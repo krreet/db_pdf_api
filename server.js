cdf1153a859f6714efa047c25bbb9e8856ce363c
Innovation Lab <reet1plus1@gmail.com>
2018-12-18 15:23:36 +0530

    enhanced pdf mAKE

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ app = express();
 var cors = require('cors');
 
 const docDefinition = {
-    content: ['This will show up in the file created by rohan']
+    content: [
+        // if you don't need styles, you can use a simple string to define a paragraph
+        'This is a Pdf created and will be sent as base 64',
+   
+        // using a { text: '...' } object lets you set styling properties
+        { text: 'This paragraph will have a bigger font', fontSize: 15 },
+   
+        // if you set pass an array instead of a string, you'll be able
+        // to style any fragment individually
+        {
+          text: [
+            'This paragraph is defined as an array of elements to make it possible to ',
+            { text: 'restyle part of it and make it bigger ', fontSize: 15 },
+            'than the rest.'
+          ]
+        }
+      ]
   };
   
  
